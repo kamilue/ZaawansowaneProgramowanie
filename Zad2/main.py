01667c72f544f67eb40d16c08534012ef8eb01bc
kamilue <kamil.plekaniec@edu.uekat.pl>
2022-10-18 18:04:19 +0200

    added classes from task2 with __str__ method

--- Zad2/main.py
+++ Zad2/main.py
@@ -1,7 +1,48 @@
-def multiplication(number1: int, number2: int) -> int:
-    return number1 * number2
+class Library:
+    city: str
+    street: str
+    zip_code: str
+    open_hours: str
+    phone: str
 
-number1 = 5
-number2 = 6
 
-multiplication(number1, number2)
+def __str__(self):
+    return f'In this class are props about Library'
+
+
+class Employee:
+    first_name: str
+    last_name: str
+    hire_date: str
+    birth_date: str
+    city: str
+    street: str
+    zip_code: str
+    phone: str
+
+
+def __str__(self):
+    return f'In this class are props about Employee'
+
+
+class Book:
+    library: str
+    publication_date: str
+    author_name: str
+    author_surname: str
+    number_of_pages: str
+
+
+def __str__(self):
+    return f'In this class are props about Book and there is library object'
+
+
+class Order:
+    employee: str
+    student: str
+    books: list
+    order_date: str
+
+
+def __str__(self):
+    return f'In this class are props about Order'
