20eeecb261ac768e5b98216f2ae06df79509bbd0
kamilue <kamil.plekaniec@edu.uekat.pl>
2022-10-18 17:57:04 +0200

    added resolved zad1

--- Zad1/main.py
+++ Zad1/main.py
@@ -1,9 +1,21 @@
-def full_name(name: str, surname:str) -> str:
-    result = f'Cześć {name} {surname}!'
-    print(result)
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+
+    def is_passed(self) -> bool:
+        sum_numbers = 0
+        for i in self.marks:
+            sum_numbers = sum_numbers + i
+        avg = sum_numbers / len(self.marks)
+        if avg > 50:
+            return True
+        return False
 
 
-name = 'Kamil'
-surname = 'Plekaniec'
-
-full_name(name, surname)
+marks = [1, 2, 3, 5, 2, 4, 5, 6]
+marks2 = [51, 55, 53, 22, 67, 60]
+student = Student('Kamil', marks)
+student2 = Student('Marcin', marks2)
+print(student.is_passed())
+print(student2.is_passed())
