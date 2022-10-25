6c30dce20a23d65102f9f98e4925556a43dc5b0c
kamilue <kamil.plekaniec@edu.uekat.pl>
2022-10-25 14:43:26 +0200

    refactor

--- Zad2/main.py
+++ Zad2/main.py
@@ -1,11 +1,12 @@
 def every_second_element(values):
-  second_values = []
+    second_values = []
 
-  for index in range(1, len(values), 2):
-    second_values.append(values[index])
+    for index in range(1, len(values), 2):
+        second_values.append(values[index])
 
-  print(second_values)
+    print(second_values)
 
-numbers = list(range(92,111))
+
+numbers = list(range(92, 111))
 
 every_second_element(numbers)
