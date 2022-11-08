e284d85446f80c629f537ade19b97d3755303dd2
kamilue <kamil.plekaniec@edu.uekat.pl>
2022-11-08 14:18:12 +0100

    added import for Classes Student

--- Zad1/main.py
+++ Zad1/main.py
@@ -1,17 +1,4 @@
-class Student:
-    def __init__(self, name, marks):
-        self.name = name
-        self.marks = marks
-
-    def is_passed(self) -> bool:
-        sum_numbers = 0
-        for i in self.marks:
-            sum_numbers = sum_numbers + i
-        avg = sum_numbers / len(self.marks)
-        if avg > 50:
-            return True
-        return False
-
+from Classes.Student import Student
 
 marks = [1, 2, 3, 5, 2, 4, 5, 6]
 marks2 = [51, 55, 53, 22, 67, 60]
