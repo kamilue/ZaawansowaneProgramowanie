2702b799475f2dce42649b65124e2892c851b8e1
kamilue <kamil.plekaniec@edu.uekat.pl>
2022-11-15 15:02:20 +0100

    refactor

--- Zad1/main.py
+++ Zad1/main.py
@@ -2,12 +2,9 @@ from flask import Flask
 from flask_restful import Api
 from routes.HomeRoute import HomeRoute
 from routes.MovieRoute import MovieRoute
-import pandas as pd
 
 app = Flask(__name__)
 api = Api(app)
-
-movies = pd.read_csv(r'C:\Users\Kamil Plekaniec\Downloads\movies.csv')
 
 api.add_resource(HomeRoute, '/')
 api.add_resource(MovieRoute, '/movies')
