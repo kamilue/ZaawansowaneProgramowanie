2f0dd517fa945168e14b11e6c9260e236917b857
kamilue <kamil.plekaniec@edu.uekat.pl>
2022-11-15 14:19:46 +0100

    initial commit

---
+++ Zad1/main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_restful import Resource, Api
+import pandas as pd
+
+app = Flask(__name__)
+api = Api(app)
+
+df = pd.read_csv(r'C:\Users\Kamil Plekaniec\Downloads\movies.csv')
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    print(df)
