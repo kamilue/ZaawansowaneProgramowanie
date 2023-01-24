bdeed0c271751e03e9ff87a4d21c7bfa44d0e275
kamilue <kamil.plekaniec@edu.uekat.pl>
2023-01-24 14:32:54 +0100

    refactor

--- person_detection/main.py
+++ person_detection/main.py
@@ -1,44 +1,68 @@
 import cv2
+import imutils
 import time
 import tkinter as tk
 from tkinter import filedialog
 
 
 def detect_people(input_file):
-    # Wczytanie pliku
-    cap = cv2.VideoCapture(input_file)
-    if not cap.isOpened():
-        print("Nie można otworzyć pliku")
-        return
-
-    # Ustawienie klasyfikatora
     hog = cv2.HOGDescriptor()
     hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+    if (input_file.endswith('jpg')):
+        # Reading the Image
+        image = cv2.imread(input_file)
 
-    # Pętla przez klatki w filmie lub pojedyncze zdjęcie
-    start_time = time.time()
-    people_count = 0
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        frame = cv2.resize(frame, (640, 480))
-        boxes, _ = hog.detectMultiScale(frame, winStride=(8, 8), padding=(32, 32), scale=1.45)
-        for (x, y, w, h) in boxes:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            people_count += 1
+        # Resizing the Image
+        image = imutils.resize(image,
+                               width=min(400, image.shape[1]))
 
-        cv2.imshow("People detection", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        # Detecting all the regions in the
+        # Image that has a pedestrians inside it
+        (regions, _) = hog.detectMultiScale(image,
+                                            winStride=(4, 4),
+                                            padding=(4, 4),
+                                            scale=1.05)
 
-    end_time = time.time()
-    cap.release()
-    cv2.destroyAllWindows()
+        # Drawing the regions in the Image
+        for (x, y, w, h) in regions:
+            cv2.rectangle(image, (x, y),
+                          (x + w, y + h),
+                          (0, 0, 255), 2)
 
-    # Wyświetlenie wyniku
-    print("Czas detekcji: {:.2f}s".format(end_time - start_time))
-    print("Liczba wykrytych osób: {}".format(people_count))
+        # Showing the output Image
+        cv2.imshow("Image", image)
+        cv2.waitKey(0)
+
+        cv2.destroyAllWindows()
+    else:
+        cap = cv2.VideoCapture(input_file)
+        while cap.isOpened():
+            ret, image = cap.read()
+            if ret:
+                image = imutils.resize(image,
+                                       width=min(400, image.shape[1]))
+
+                (regions, _) = hog.detectMultiScale(image,
+                                                    winStride=(4, 4),
+                                                    padding=(4, 4),
+                                                    scale=1.05)
+
+                # Drawing the regions in the
+                # Image
+                for (x, y, w, h) in regions:
+                    cv2.rectangle(image, (x, y),
+                                  (x + w, y + h),
+                                  (0, 0, 255), 2)
+
+                # Showing the output Image
+                cv2.imshow("Image", image)
+                if cv2.waitKey(25) & 0xFF == ord('q'):
+                    break
+            else:
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
 
 
 def open_file_explorer():
