80e01c44110a889ca76d00ba10476f67b6449b84
kamilue <kamil.plekaniec@edu.uekat.pl>
2023-01-24 12:10:44 +0100

    refactor

--- person_detection/main.py
+++ person_detection/main.py
@@ -1,15 +1,24 @@
+from flask import Flask, request
 import cv2
+import numpy as np
+import requests
 import time
-import tkinter as tk
-from tkinter import filedialog
+
+app = Flask(__name__)
 
 
-def detect_people(input_file):
-    # Wczytanie pliku
-    cap = cv2.VideoCapture(input_file)
-    if not cap.isOpened():
-        print("Nie można otworzyć pliku")
-        return
+@app.route('/detect_people', methods=['GET'])
+def detect_people_api():
+    url = request.args.get('url')
+    people_count, detection_time = detect_people(url)
+    return f"Detekcja zakończona. Czas detekcji: {detection_time:.2f}s. Liczba wykrytych osób: {people_count}"
+
+
+def detect_people(url):
+    # Pobieranie pliku
+    response = requests.get(url)
+    img_array = np.array(bytearray(response.content), dtype=np.uint8)
+    frame = cv2.imdecode(img_array, -1)
 
     # Ustawienie klasyfikatora
     hog = cv2.HOGDescriptor()
@@ -18,36 +27,20 @@ def detect_people(input_file):
     # Pętla przez klatki w filmie lub pojedyncze zdjęcie
     start_time = time.time()
     people_count = 0
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
 
-        frame = cv2.resize(frame, (640, 480))  # zmniejszenie rozdzielczości klatek
-        boxes, _ = hog.detectMultiScale(frame, winStride=(8, 8), padding=(32, 32), scale=1.1)
-        # zwiększenie skali detekcji
-        for (x, y, w, h) in boxes:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            people_count += 1
-
+    frame = cv2.resize(frame, (640, 480))  # zmniejszenie rozdzielczości klatek
+    boxes, _ = hog.detectMultiScale(frame, winStride=(8, 8), padding=(32, 32), scale=1.1)
+    # zwiększenie skali detekcji
+    for (x, y, w, h) in boxes:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        people_count += 1
         cv2.imshow("People detection", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    cv2.destroyAllWindows()
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
     end_time = time.time()
-    cap.release()
-    print("Czas detekcji: {:.2f}s".format(end_time - start_time))
-    print("Liczba wykrytych osób: {}".format(people_count))
-    root = tk.Tk()
-    root.withdraw()
-    root.destroy()
+    detection_time = end_time - start_time
+    return people_count, detection_time
 
 
-def open_file_explorer():
-    root = tk.Tk()
-    root.withdraw()
-    input_file = filedialog.askopenfilename()
-    detect_people(input_file)
-
-
-open_file_explorer()
+if __name__ == '__main__':
+    app.run(debug=True)
