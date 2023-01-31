2300cd508ffdbc8065ef7605b889ee65d28017e6
kamilue <kamil.plekaniec@edu.uekat.pl>
2023-01-31 22:12:14 +0100

    refactor

--- person_detection/main.py
+++ person_detection/main.py
@@ -9,15 +9,11 @@ def detect_people(input_file):
     hog = cv2.HOGDescriptor()
     hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
     if (input_file.endswith('jpg')):
-        # Reading the Image
         image = cv2.imread(input_file)
 
-        # Resizing the Image
         image = imutils.resize(image,
                                width=min(400, image.shape[1]))
 
-        # Detecting all the regions in the
-        # Image that has a pedestrians inside it
         (regions, _) = hog.detectMultiScale(image,
                                             winStride=(4, 4),
                                             padding=(4, 4),
@@ -25,18 +21,15 @@ def detect_people(input_file):
 
         start_time = time.time()
         people_count = 0
-        # Drawing the regions in the Image
         for (x, y, w, h) in regions:
             cv2.rectangle(image, (x, y),
                           (x + w, y + h),
                           (0, 0, 255), 2)
             people_count += 1
 
-        # Showing the output Image
         cv2.imshow("People detection", image)
         end_time = time.time()
         cv2.waitKey(0)
-        # Wyświetlenie wyniku
         print("Czas detekcji: {:.2f}s".format(end_time - start_time))
         print("Liczba wykrytych osób: {}".format(people_count))
         cv2.destroyAllWindows()
@@ -55,16 +48,12 @@ def detect_people(input_file):
                                                     padding=(4, 4),
                                                     scale=1.05)
 
-                # Drawing the regions in the
-
-                # Image
                 for (x, y, w, h) in regions:
                     cv2.rectangle(image, (x, y),
                                   (x + w, y + h),
                                   (0, 0, 255), 2)
                     people_count += 1
 
-                # Showing the output Image
                 cv2.imshow("People detection", image)
                 if cv2.waitKey(25) & 0xFF == ord('q'):
                     break
@@ -75,7 +64,6 @@ def detect_people(input_file):
         cap.release()
         cv2.destroyAllWindows()
 
-        # Wyświetlenie wyniku
         print("Czas detekcji: {:.2f}s".format(end_time - start_time))
         print("Liczba wykrytych osób: {}".format(people_count))
 
